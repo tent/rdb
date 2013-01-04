39d319a25c16ba278d57cff01611a0eff76bed3e
Jonathan Rudenberg <jonathan@titanous.com>
2013-01-03 21:52:24 -0500

    examples/diff: Parse -> Decode

--- examples/diff.go
+++ examples/diff.go
@@ -1,4 +1,4 @@
-// This is a very basic example of a program that implements rdb.Parser and
+// This is a very basic example of a program that implements rdb.decoder and
 // outputs a human readable diffable dump of the rdb file.
 package main
 
@@ -9,70 +9,70 @@ import (
 	"github.com/titanous/rdb"
 )
 
-type parser struct {
+type decoder struct {
 	db int
 	i  int
 }
 
-func (p *parser) StartRDB() {
+func (p *decoder) StartRDB() {
 }
 
-func (p *parser) StartDatabase(n int) {
+func (p *decoder) StartDatabase(n int) {
 	p.db = n
 }
 
-func (p *parser) EndDatabase(n int) {
+func (p *decoder) EndDatabase(n int) {
 }
 
-func (p *parser) EndRDB() {
+func (p *decoder) EndRDB() {
 }
 
-func (p *parser) Set(key, value []byte, expiry int64) {
+func (p *decoder) Set(key, value []byte, expiry int64) {
 	fmt.Printf("db=%d %q -> %q\n", p.db, key, value)
 }
 
-func (p *parser) StartHash(key []byte, length, expiry int64) {
+func (p *decoder) StartHash(key []byte, length, expiry int64) {
 }
 
-func (p *parser) Hset(key, field, value []byte) {
+func (p *decoder) Hset(key, field, value []byte) {
 	fmt.Printf("db=%d %q . %q -> %q\n", p.db, key, field, value)
 }
 
-func (p *parser) EndHash(key []byte) {
+func (p *decoder) EndHash(key []byte) {
 }
 
-func (p *parser) StartSet(key []byte, cardinality, expiry int64) {
+func (p *decoder) StartSet(key []byte, cardinality, expiry int64) {
 }
 
-func (p *parser) Sadd(key, member []byte) {
+func (p *decoder) Sadd(key, member []byte) {
 	fmt.Printf("db=%d %q { %q }\n", p.db, key, member)
 }
 
-func (p *parser) EndSet(key []byte) {
+func (p *decoder) EndSet(key []byte) {
 }
 
-func (p *parser) StartList(key []byte, length, expiry int64) {
+func (p *decoder) StartList(key []byte, length, expiry int64) {
 	p.i = 0
 }
 
-func (p *parser) Rpush(key, value []byte) {
+func (p *decoder) Rpush(key, value []byte) {
 	fmt.Printf("db=%d %q[%d] -> %q\n", p.db, key, p.i, value)
 	p.i++
 }
 
-func (p *parser) EndList(key []byte) {
+func (p *decoder) EndList(key []byte) {
 }
 
-func (p *parser) StartZSet(key []byte, cardinality, expiry int64) {
+func (p *decoder) StartZSet(key []byte, cardinality, expiry int64) {
 	p.i = 0
 }
 
-func (p *parser) Zadd(key []byte, score float64, member []byte) {
+func (p *decoder) Zadd(key []byte, score float64, member []byte) {
 	fmt.Printf("db=%d %q[%d] -> {%q, score=%g}\n", p.db, key, p.i, member, score)
 	p.i++
 }
 
-func (p *parser) EndZSet(key []byte) {
+func (p *decoder) EndZSet(key []byte) {
 }
 
 func maybeFatal(err error) {
@@ -85,6 +85,6 @@ func maybeFatal(err error) {
 func main() {
 	f, err := os.Open(os.Args[1])
 	maybeFatal(err)
-	err = rdb.Parse(f, &parser{})
+	err = rdb.Decode(f, &decoder{})
 	maybeFatal(err)
 }
