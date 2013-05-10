5c1fc6e1331803c097e9d1d2a516dcdf20d92663
Jonathan Rudenberg <jonathan@titanous.com>
2013-05-10 10:59:54 -0400

    Implement NopDecoder

--- examples/diff.go
+++ examples/diff.go
@@ -12,43 +12,23 @@ import (
 type decoder struct {
 	db int
 	i  int
-}
-
-func (p *decoder) StartRDB() {
+	rdb.NopDecoder
 }
 
 func (p *decoder) StartDatabase(n int) {
 	p.db = n
 }
 
-func (p *decoder) EndDatabase(n int) {
-}
-
-func (p *decoder) EndRDB() {
-}
-
 func (p *decoder) Set(key, value []byte, expiry int64) {
 	fmt.Printf("db=%d %q -> %q\n", p.db, key, value)
-}
-
-func (p *decoder) StartHash(key []byte, length, expiry int64) {
 }
 
 func (p *decoder) Hset(key, field, value []byte) {
 	fmt.Printf("db=%d %q . %q -> %q\n", p.db, key, field, value)
 }
 
-func (p *decoder) EndHash(key []byte) {
-}
-
-func (p *decoder) StartSet(key []byte, cardinality, expiry int64) {
-}
-
 func (p *decoder) Sadd(key, member []byte) {
 	fmt.Printf("db=%d %q { %q }\n", p.db, key, member)
-}
-
-func (p *decoder) EndSet(key []byte) {
 }
 
 func (p *decoder) StartList(key []byte, length, expiry int64) {
@@ -60,9 +40,6 @@ func (p *decoder) Rpush(key, value []byte) {
 	p.i++
 }
 
-func (p *decoder) EndList(key []byte) {
-}
-
 func (p *decoder) StartZSet(key []byte, cardinality, expiry int64) {
 	p.i = 0
 }
@@ -70,9 +47,6 @@ func (p *decoder) StartZSet(key []byte, cardinality, expiry int64) {
 func (p *decoder) Zadd(key []byte, score float64, member []byte) {
 	fmt.Printf("db=%d %q[%d] -> {%q, score=%g}\n", p.db, key, p.i, member, score)
 	p.i++
-}
-
-func (p *decoder) EndZSet(key []byte) {
 }
 
 func maybeFatal(err error) {
